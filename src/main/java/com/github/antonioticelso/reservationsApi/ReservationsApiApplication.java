0f9483de7b13986fbb4059662e2be2e5d0c1f4b2
Antonio <antonio.ti.celso@gmail.com>
2022-11-09 10:50:26 -0300

    First commit - Reservations Api

---
+++ src/main/java/com/github/antonioticelso/reservationsApi/ReservationsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.reservationsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/antonioticelso/reservationsApi/ReservationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.reservationsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
